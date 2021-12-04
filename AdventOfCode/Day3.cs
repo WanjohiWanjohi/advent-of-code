c24d74245f72f82a6767490530ec677c53cd4f70
Wanjohi Wanjohi <wamuyuwanjohi97@gmail.com>
2021-12-04 14:34:44 +0300

    Day 3 : Initial Commit

---
+++ AdventOfCode/Day3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using AdventOfCode;
+
+public class Day3 : InputClass   
+{
+  
+    public string resourceUrl { get; set; }
+
+
+    public Day3(string ResourceURL) : base()
+    {
+        resourceUrl = ResourceURL;
+    }
+
+    public string[] Day3Challenge(string ResourceUrl)
+    {
+         HttpClient httpClient =base.CreateHttpClient();
+        httpClient.BaseAddress = new Uri(resourceUrl);
+        Task<string[]> urlContent = base.GetContentAsync(ResourceUrl, httpClient);
+        return urlContent.Result;
+
+    }
+
+
+    public IEnumerable<string> TransformInput(string[] content , int startIndex , int endIndex)
+    {
+        List<string> verticalItems = new List<string>();
+        foreach (string item in content)
+        {
+            if (item.Length > 0)
+            {
+                var newItem = (string)item.Substring(startIndex, endIndex);
+                verticalItems.Add(newItem);
+            }
+        }
+        return verticalItems;
+    }
+    public void GetMostandLeastCommon(IEnumerable<string> positionArray)
+    {
+            var most = positionArray.GroupBy(k => k).OrderByDescending(grp => grp.Count())
+              .Select(grp => grp.Key).First();
+            var least  = positionArray.GroupBy(i => i).OrderByDescending(grp => grp.Count())
+              .Select(grp => grp.Key).Last();
+       
+    }
+}
+
+
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -5,13 +5,27 @@ using AdventOfCode;
 using System.Collections;
 using Newtonsoft.Json;
 
-//Day 2 Challenge: Part 1 
-string Day2Url =  "https://adventofcode.com/2021/day/2/input";
+//Day 2 Challenge: Part 1
+//
+
+/*string Day2Url =  "https://adventofcode.com/2021/day/2/input";
 Day2 day2 = new Day2(Day2Url);
 var content = day2.Day2Challenge(Day2Url);
 var KeyPairs = day2.SerializeintoKeyValue(content);
 var solutionList = day2.ChallengeSolution(KeyPairs);
 var answer = day2.MultiplyPosition(solutionList);
-Console.WriteLine(answer);
+Console.WriteLine(answer);*/
+
+// Day 3 Cahllenge
+
+string Day3Url = "https://adventofcode.com/2021/day/3/input";
+Day3 day3 = new Day3(Day3Url);
+var content = day3.Day3Challenge(Day3Url);
+for (int i = 0; i < 13; i++)
+{
+    var positionArray = day3.TransformInput(content, i, 1);
+
+}
 
 
+    Console.WriteLine(content);
